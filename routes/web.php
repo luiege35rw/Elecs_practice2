b298692e16048f9eae0904f89c220c1d20f585a1
Fujita Namiko <fujita-namiko@elecs-web.co.jp>
2022-05-16 15:43:53 +0900

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// お問い合わせ入力ページ
+Route::get('/', 'ContactsController@index')->name('contact');
+
+// 確認ページ
+Route::post('/confirm', 'ContactsController@confirm')->name('confirm');
+
+// DB挿入、メール送信
+Route::post('/process', 'ContactsController@process')->name('process');
+
+// 完了ページ
+Route::get('/complete', 'ContactsController@complete')->name('complete');
